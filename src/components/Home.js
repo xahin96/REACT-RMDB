e916e959b6464212008bc71380350e4d6e9bd7b9
Shahriar Rahman <zahin@Shahriars-MacBook-Air.local>
2022-01-08 18:34:06 +0600

    First commit

---
+++ src/components/Home.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+//config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BAsE_URL, IMAGE_BASE_URL } from '../config';
+//components
+import HeroImage from "./HeroImage";
+//hook
+import { useHomeFetch } from  '../hooks/useHomeFetch';
+//image
+import NoImage from '../images/no_image.jpg';
+
+const Home = () => {
+    
+    const { state, loading, error } = useHomeFetch();
+    console.log(state);
+
+    return (
+        <>
+            {state.results[0] ?
+                <HeroImage
+                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                    title={state.results[0].original_title}
+                    text={state.results[0].overview}
+                />
+                : null
+            }
+        </>
+    )
+}
+
+export default Home;
