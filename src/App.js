fd02bfa73cb930e6063523539954a1a1d24bc0f4
Shahriar Rahman <zahin@Shahriars-MacBook-Air.local>
2022-01-09 23:56:19 +0600

    Shahriar | master | Router added to main app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
+//Routing
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 //components
 import Header from './components/Header';
-import Home from './components/Home'
+import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
 
 //styles
 import { GlobalStyle } from './GlobalStyle'
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Home/>
-      <GlobalStyle />
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home />}> </Route>
+      <Route path='/:movieId' element={<Movie />}> </Route>
+      <Route path='/*' element={<NotFound j/>}> </Route>
+    </Routes>
+    <GlobalStyle />
+  </Router>
+);
 
 export default App;
